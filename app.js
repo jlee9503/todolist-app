10c47af4d8cc8ee99082f6afc600c017634b315b
jlee9503 <jungsu9503@gmail.com>
2021-04-26 23:47:10 -0400

    Add JS features

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
 // Selectors
-const addTodoBtn = document.querySelector('.addTodo');
+const addTodoBtn = document.querySelector(".addTodo");
+const createNewTask = document.querySelector(".addNewTask");
+const content = document.querySelector(".content");
 
+// Functions
+const openNewTask = () => {
+	createNewTask.classList.toggle("hide");
+};
+
+const showNewTask = () => {
+	// Create List div tag
+	const ListDiv = document.createElement("div");
+	ListDiv.classList.add("newList");
+
+	// Create an input field
+	const ListInput = document.createElement("input");
+	ListInput.classList.add("newListInput");
+	// Add type='text' attribute
+	const inputType = document.createAttribute("type");
+	inputType.value = "text";
+	ListDiv.appendChild(ListInput);
+
+	// Append the new list div into the <header> tag
+	content.appendChild(ListDiv);
+};
+
+// Event Listeners
+// addTodoBtn.addEventListener("click", showNewTask);
